40512b5f469f9698be54cccdc7ec1636f6faea3a
Matheus Renzo <matheusrenzo@matheuss-macbook-pro.local>
2018-06-21 20:30:39 -0300

    Initial Commit

---
+++ 2do/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2do
+//
+//  Created by Matheus Renzo on 6/21/18.
+//  Copyright © 2018 Matheus Renzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
